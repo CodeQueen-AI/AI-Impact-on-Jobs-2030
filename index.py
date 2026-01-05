e13aeb1a2bda37f2adabd27ce69fb916c6b48776
CodeQueen-AI <codeq209@gmail.com>
2026-01-05 15:38:41 +0500

    update

--- index.py
+++ index.py
@@ -70,12 +70,9 @@ print(df.isnull().sum())
 # plt.show()
 
 # AI Exposure Index by Education Level
-plt.figure()
-sns.boxplot(
-    x="Education_Level",
-    y="AI_Exposure_Index",
-    data=df
-)
+plt.figure(figsize=(10,6))
+education_colors = ["#FF5733", "#33FF57", "#FFC300", "#FF33A6"]
+sns.boxplot(x="Education_Level", y="AI_Exposure_Index", data=df, palette=education_colors)
 plt.title("AI Exposure Index by Education Level")
 plt.xlabel("Education Level")
 plt.ylabel("AI Exposure Index")
