4c649a5f1a7f7dc9f90f97f5ed836fb26c2f7184
CodeQueen-AI <codeq209@gmail.com>
2026-01-05 14:59:37 +0500

    update'

--- index.py
+++ index.py
@@ -29,30 +29,30 @@ print(df.isnull().sum())
 # EDA RREAL ANALYSIS
 
 # Risk Category distribution
-sns.countplot(x='Risk_Category' , data=df)
-# No plor show
+# sns.countplot(x='Risk_Category' , data=df)
+# # No plor show
 
-# Automation probability distribution
-sns.histplot(df['Automation_Probability_2030'])
+# # Automation probability distribution
+# sns.histplot(df['Automation_Probability_2030'])
 
-# Top 10 Risky Jobs
-top_risk_jobs = df.sort_values(...).head(10)
+# # Top 10 Risky Jobs
+# top_risk_jobs = df.sort_values(...).head(10)
 
-# Salary vs Automation Risk
-sns.scatterplot(x='Average_Salary' , y='Automation_Probability_2030')
+# # Salary vs Automation Risk
+# sns.scatterplot(x='Average_Salary' , y='Automation_Probability_2030')
 
-# Education level vs AI exposure
-sns.box(x="Education_Level" , y='AI_Exposure_Index')
+# # Education level vs AI exposure
+# sns.box(x="Education_Level" , y='AI_Exposure_Index')
 
-# Tech Growth vs AI Exposure
-sns.scatterplot(x='Tech_Growth_Factor' , y='AI_Exposure_Index')
+# # Tech Growth vs AI Exposure
+# sns.scatterplot(x='Tech_Growth_Factor' , y='AI_Exposure_Index')
 
-# NUMPY ANALYSIS
+# # NUMPY ANALYSIS
 
-# Automation Risk Numbers
-np.mean(...)
-np.max(...)
-np.min(...)
+# # Automation Risk Numbers
+# np.mean(...)
+# np.max(...)
+# np.min(...)
 
-# Conclusion Print
-print('AI Impact Varies')
+# # Conclusion Print
+# print('AI Impact Varies')
